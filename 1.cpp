63ec844be008e5e50e57bd330037867ebf04010a
zcsfish <zcsfish@qq.com>
2024-10-28 21:16:55 +0800

    Commit at 2024-10-28 21:16

--- 1.cpp
+++ 1.cpp
@@ -6,7 +6,7 @@ int main() {
     system("git add .");
 
     // 执行 'git commit -m "修改文件"' 命令
-    system("git commit -m \"$修改文件(date +%Y-%m-%d %H:%M)\"");
+    system("git commit -m \"Commit at $(date +%Y-%m-%d\\ %H:%M)\"");
 
     // 执行 'git push -u origin main' 命令
     system("git push -u origin main");
